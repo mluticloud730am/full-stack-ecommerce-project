628601689dc4532f2c4c7e759e874b5cb89b1eea
root <root@ip-172-31-28-229.ec2.internal>
2026-04-25 06:37:24 +0000

    Initial commit - full stack ecommerce project

--- backend/spring-boot-ecommerce/src/main/java/com/codecafe/ecommerce/persistence/config/RepositoryRestConfig.java
+++ backend/spring-boot-ecommerce/src/main/java/com/codecafe/ecommerce/persistence/config/RepositoryRestConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import com.codecafe.ecommerce.persistence.entity.Product;
 import com.codecafe.ecommerce.persistence.entity.ProductCategory;
@@ -21,53 +22,48 @@ public class RepositoryRestConfig implements RepositoryRestConfigurer {
     
     private EntityManager entityManager;
     
-    // constructor injection - JPA Entity Manager
+    // constructor injection
     @Autowired
     public RepositoryRestConfig(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
     
     @Override
-    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+    public void configureRepositoryRestConfiguration(
+            RepositoryRestConfiguration config,
+            CorsRegistry cors) {
 
         HttpMethod[] unsupportedHttpMethods = { HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE };
 
-        // disable HTTP methods for Products: PUT, POST and DELETE
+        // disable HTTP methods for Product
         config.getExposureConfiguration()
               .forDomainType(Product.class)
               .withItemExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedHttpMethods))
               .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedHttpMethods));
 
-        // disable HTTP methods for ProductCategory: PUT, POST and DELETE
+        // disable HTTP methods for ProductCategory
         config.getExposureConfiguration()
               .forDomainType(ProductCategory.class)
               .withItemExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedHttpMethods))
               .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedHttpMethods));
-        
-        // call an internal helper method
+
+        // expose entity IDs
         exposeIds(config);
     }
 
-
-    // entity ids (category ids) will be available in /api/product-category endpoint
+    // expose all entity IDs dynamically
     private void exposeIds(RepositoryRestConfiguration config) {
-        // expose entity ids
 
-        // get a list of all entity classes from the entity manager
         Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
 
-        // create a list of the entity types
-        List<Class> entityClasses = new ArrayList<>();
+        List<Class<?>> entityClasses = new ArrayList<>();
 
-        // get the entity types of the entities
-        for(EntityType entityType : entities) {
+        for (EntityType<?> entityType : entities) {
             entityClasses.add(entityType.getJavaType());
         }
 
-        // expose the entity ids for the array of entity/domain types
-        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        Class<?>[] domainTypes = entityClasses.toArray(new Class<?>[0]);
+
         config.exposeIdsFor(domainTypes);
-
     }
-
 }
--- frontend/angular-ecommerce/src/environments/environment.prod.ts
+++ frontend/angular-ecommerce/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: "http://35.172.215.84:8080/api"
 };
